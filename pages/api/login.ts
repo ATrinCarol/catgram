6f9038dd6c7e086d5792d068fa095d8c93d90987
Carol Trindade <caroline.alvestrindade@hotmail.com>
2023-02-01 03:28:59 -0300

    versão inicial do login

---
+++ pages/api/login.ts
@@ -0,0 +1,18 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+import { NextRequest, NextResponse } from 'next/server';
+
+export default(
+    req : NextApiRequest,
+    res: NextApiResponse
+) => {
+    if(req.method == 'POST'){
+        const {login, senha} = req.body;
+
+        if (login === 'admin@admin.com' &&
+            senha === 'Admin@123'){
+                res.status(200).json({msg: 'Usuário autenticado com sucesso!'})
+            }
+            return res.status(400).json({erro: 'Usuário ou senha incorreta'});
+    }
+    return res.status(405).json({erro: 'Método informado inválido'});
+}
